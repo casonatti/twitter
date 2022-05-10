df7123aaa4626f498ead5ebc31b233a617fed823
Jeison Caroly <jeison.casonatti@gmail.com>
2022-05-10 18:10:06 -0300

    implemented some robustness on client side

--- client.cpp
+++ client.cpp
@@ -1,3 +1,4 @@
+#include <csignal>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
@@ -9,21 +10,48 @@
 
 using namespace std;
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 512
 #define IP_PROTOCOL 0
 #define MESSAGE_LENGTH 128
 #define PORT 8080
+#define STRING_QUIT "QUITTING"
+
+//global var
+int sock = 0;
+
+
+void signalHandler(int signum) {
+    const char* exit_message = "quit SOMETHING GONE WRONG WITH ME... CYA!";
+    char buffer[BUFFER_SIZE] = { 0 };
+
+    memset(buffer, ' ', strlen(buffer));
+        
+    cout << "sending message to server: \"" << exit_message << "\"" << endl;
+    send(sock, exit_message, strlen(exit_message), 0);
+        
+    recv(sock, buffer, BUFFER_SIZE, 0);
+    cout << "received message from server: \"" << buffer << "\"" << endl << endl;
+
+    close(sock);
+
+    exit(signum);
+}
 
 int main() {
+    bool flag_quit = false;
     char in[MESSAGE_LENGTH] = { 0 };
-    int sock = 0, valread;
+    int valread, connection = -1;
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE] = { 0 };
+    string str_aux;
+    signal(SIGINT, signalHandler);
 
     if((sock = socket(AF_INET, SOCK_STREAM, IP_PROTOCOL)) <  0) {
         printf("Socket creation error");
         return -1;
     }
+
+    cout << sock << endl;
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
@@ -38,7 +66,7 @@ int main() {
         return -1;
     }
     
-    while(1) {
+    while(!flag_quit) {
         //buffer reset
         memset(buffer, ' ', strlen(buffer));
         
@@ -47,9 +75,19 @@ int main() {
         cout << "sending message to server: \"" << in << "\"" << endl;
         send(sock, in, strlen(in), 0);
         
-        valread = read(sock, buffer, BUFFER_SIZE);
+        valread = recv(sock, buffer, BUFFER_SIZE, 0);
         cout << "received message from server: \"" << buffer << "\"" << endl << endl;
+
+        str_aux = buffer;
+
+        if(str_aux.compare(0,4,STRING_QUIT,0,4) == 0) {
+            flag_quit = true;
+        }
     }
+
+    close(sock);
+
+    cout << "Connection is closed." << endl << endl;
 
     return 0;
 }
